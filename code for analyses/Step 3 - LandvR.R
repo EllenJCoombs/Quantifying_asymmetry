71bb92930a837189f89589099c32a3756c975cf9
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2021-11-16 12:45:12 +0000

    Update Step 3 - LandvR.R

--- code for analyses/Step 3 - LandvR.R
+++ code for analyses/Step 3 - LandvR.R
@@ -1,6 +1,11 @@
 
+#Qauntifying landmark displacement 
 
-#==========
+####################################
+#                                  #
+#   Step 3 - Coombs and Felice     #
+#                                  #
+####################################
 
 if(!require(devtools)) install.packages("devtools")
 library(devtools)
@@ -12,14 +17,14 @@ library(landvR)
 Mirrored_skull_AC[1,,2] == Full_skull_AB[1,,2]
 # FALSE FALSE FALSE
 ## This is due to rounding, in fact they have the same 9 digits - round them 
-round(Mirrored_skull_AC[1,,2], digits = 9) == round(FUll_skull_AB[1,,2], digits = 9)
+round(Mirrored_skull_AC[1,,2], digits = 9) == round(Manual_skull_AB[1,,2], digits = 9)
 # TRUE TRUE TRUE
 
 #I’ve updated landvR to version 0.3 where the coordinates.difference function now have a tolerance optional argument.
 #You can use the following to get the 0 difference results:
 
 differences_between_lms <- coordinates.difference(coordinates = Mirrored_skull_AC[,,65],
-                                                  reference = Full_skull_AB[,,65],
+                                                  reference = Manual_skull_AB[,,65],
                                                   type = "spherical",
                                                   rounding = 9)
 
@@ -32,22 +37,33 @@ colfunc <- colorRampPalette(c("red", "yellow", "white"))
 colfunc(10)
 plot(rep(1,10),col=colfunc(10),pch=19,cex=3)
 
-get.col.spectrum <- landvR::procrustes.var.plot(Full_skull_AB[,,65], Mirrored_skull_AC[,,65], col.val = differences_between_lms[[1]][,1], col = colfunc)
+get.col.spectrum <- landvR::procrustes.var.plot(Manual_skull_AB[,,65], Mirrored_skull_AC[,,65], col.val = differences_between_lms[[1]][,1], col = colfunc)
 
 
 test=differences_between_lms[[1]][,1] #this is a test for specimen 1 to look at the differences between lms 
 test
 
 ##### LOOKING AT AN AVERAGE SPECIMEN ######
-N=123 #number of landmarks 
-specs=162 #number of specimens 
-all_combined=array(dim=c(N,3,specs)) #3 is the columns of data we need (radii, azimuth, polar)
+
+library(landvR)
+N=123 #number of total landmarks 
+nfixed = 66 #number of fixed landmarks 
+specs = 157 #number of specimens 
+k = 3 #number of dimensions in the matrix
+colfunc <- colorRampPalette(c("red", "yellow", "white")) #create colour function for visualising landmarks
+colfunc(10) #choose number of increments for colour scale 
+
+#make the array for analyses: 
+all_combined = array(dim=c(N,3,specs)) #3 is the columns of data we need (radii, azimuth, polar)
+#manual_data is the fully landmarked skull (reference data)
+#mirrored_data is the half-landmarked skull that has been mirrored 
+#calculate the coordinates.differences between these data sets (i.e. how much the landmarks move between the manually placed landmarks and the mirrored landmarks) 
 
 i=1
 for (i in 1:specs)
 {
   all_differences <- coordinates.difference(coordinates = Mirrored_skull_AC[,,i],
-                                            reference = Full_skull_AB[,,i],
+                                            reference = Manual_skull_AB[,,i],
                                             type = "spherical",
                                             rounding = 9)
   
@@ -57,19 +73,18 @@ for (i in 1:specs)
 }
 
 
-#55, 56, 57, 59, 60 are all missing data and should be zero 
-all_combined[1:66, 1:3, 1:162] <- c(0.000000, 0.000000, 0.000000)
-#write.csv(all_combined, file = 'all_combined.csv')
+#landmarks 1:66 (nfixed) in this example are fixed and therefore have the value of zero 
+all_combined[1:nfixed, 1:k, 1:specs] <- c(0.000000, 0.000000, 0.000000)
 
-radii=all_combined[,1,] #looking at the second column (usually x,y,z) but here it is the radii, aziumuth, and polar 
-radii_mean=apply(radii, c(1), mean) #c(1) looking at the first column which is the radii 
-#test=all_combined[[1]][,,1] #this is a test for specimen 1 to look at the differences between lms 
-#test
+#save output if desired using: write.csv(all_combined, file = 'all_combined.csv')
 
-radii=all_combined[,1,] #second column of whole dataset with just the radii [,1,]
+radii=all_combined[,1,] #radius per landmark for each specimen (second column of whole dataset with just the radii [,1,])
 
+radii_mean=apply(radii, c(1), mean) #c(1) look at the first column - the radii 
+#radii_mean is a mean radius value per landmark 
 
-#Looking at the average radii compared to specimen 21 (or an average specimen)
-get.col.spectrum <- landvR::procrustes.var.plot(Full_skull_AB[,,9], Mirrored_skull_AC[,,9], col.val = radii_mean, col = colfunc)
-#datcol2<-c(rep("black",66),get.col.spectrum)
-#open3d()
+#save radii and radii_mean as .csv files for further analyses 
+
+#example
+#looking at the average radii compared to specimen 21 (or choose an average specimen if preferred)
+get.col.spectrum <- landvR::procrustes.var.plot(Manual_skull_AB[,,21], Mirrored_skull_AC[,,21], col.val = radii_mean, col = colfunc)
